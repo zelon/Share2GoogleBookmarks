186768eacf53264ce714e239a71ad062ec1e41cb
zelon <zelonion@gmail.com>
2012-01-14 23:41:32 +0900

    Remove needless codes

--- Share2GoogleBookmarks/src/com/wimy/share2googlebookmarks/Share2GoogleBookmarksActivity.java
+++ Share2GoogleBookmarks/src/com/wimy/share2googlebookmarks/Share2GoogleBookmarksActivity.java
@@ -1,12 +1,11 @@
 package com.wimy.share2googlebookmarks;
 
-import java.util.Set;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 public class Share2GoogleBookmarksActivity extends Activity {
     /** Called when the activity is first created. */
@@ -19,16 +18,19 @@ public class Share2GoogleBookmarksActivity extends Activity {
 
 		if ( bundle != null )
 		{
-			Set<String> keyset = bundle.keySet();
-			
-			for ( String s : keyset )
-			{
-				Log.i("gm", bundle.get(s).toString());
-			}
-			
 			String url = bundle.getString(Intent.EXTRA_TEXT);
 			String subject = bundle.getString(Intent.EXTRA_SUBJECT);
 
+			if ( url == null && subject == null )
+			{
+				Toast.makeText(this, "url and subject are null", Toast.LENGTH_LONG).show();
+				finish();
+				return;
+			}
+			
+			if ( url == null ) url = "unknown url";
+			if ( subject == null ) subject = "unknown subject";
+			
 			String newUrl = String.format("https://www.google.com/bookmarks/mark?op=edit&bkmk=%s&title=%s", url, subject);
 
 			Log.i("gm", "Load " + newUrl);
